9d59c2233e99cbe6cc9c508386bfa3ebdb5efe20
nikitaabuya <kittaabuya@gmail.com>
2024-11-27 23:44:59 +0300

    fixing plumber activity

--- app/src/main/java/com/example/nairobigo/PlumberActivity.kt
+++ app/src/main/java/com/example/nairobigo/PlumberActivity.kt
@@ -1,6 +1,7 @@
 package com.example.nairobigo
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,13 @@ class PlumberActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_plumber)
+
+        // Apply window insets for edge-to-edge support
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        }
     }
-}
